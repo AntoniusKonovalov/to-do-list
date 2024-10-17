14ea9e2b0daa9aefdee9702822fade272791a6cb
Anton Konovalov <antoniuskonovalov@gmail.com>
2024-10-17 09:34:20 +0200

    adding the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,9 @@
-export { auth as middleware } from "@/auth";
-
-
-
+import { auth } from "@/auth"
+ 
+export default auth((req) => {
+  console.log("ROUTE: ", req.nextUrl.pathname)
+})
+ 
+export const config = {
+  matcher: ['/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',],
+}
